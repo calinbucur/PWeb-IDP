cdd1bc4b108ede11a4d4fef17a8691f011864002
Carapcea Andrei-Antonio <andy070799@gmail.com>
2022-05-20 15:45:17 +0300

    update lintrc

--- pweb_frontend/.eslintrc.js
+++ pweb_frontend/.eslintrc.js
@@ -1,24 +1,23 @@
+/* eslint-disable no-undef */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    'jest'
-  ],
-  rules: {
-  }
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 }
