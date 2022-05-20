86ae7b9134cddec3ed4fb00814ad74a26af133de
Carapcea Andrei-Antonio <andy070799@gmail.com>
2022-05-20 15:43:59 +0300

    make linter happy

--- pweb_frontend/src/Feed.js
+++ pweb_frontend/src/Feed.js
@@ -1,19 +1,18 @@
-import React from "react";
-import "./Feed.css"
+import React from 'react'
+import './Feed.css'
 
 const Feed = () => {
-    let objs = []
-    for (let i = 0; i < 20; i++) {
-        if (i === 4) {
-            objs.push(<div className = "Feed-item Feed-highlight" key = {i}></div>)
-        } else
-        objs.push(<div className = "Feed-item" key = {i}></div>)
-    }
-    return (
+  const objs = []
+  for (let i = 0; i < 20; i++) {
+    if (i === 4) {
+      objs.push(<div className = "Feed-item Feed-highlight" key = {i}></div>)
+    } else { objs.push(<div className = "Feed-item" key = {i}></div>) }
+  }
+  return (
         <div className = "Feed">
             {objs}
         </div>
-    );
-};
+  )
+}
 
-export default Feed;
+export default Feed
