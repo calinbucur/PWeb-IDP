b6135eefcf5c19dd02005455b32c137a0f24d6db
Carapcea Andrei-Antonio <andy070799@gmail.com>
2022-05-20 15:36:18 +0300

    fix merge conflicts

--- pweb_frontend/src/LoginButton.js
+++ pweb_frontend/src/LoginButton.js
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import './BannerButton.css'
-=======
-import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
->>>>>>> backupidp
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0()
 
-<<<<<<< HEAD
   return <button className = "BannerButton" onClick={() => loginWithRedirect()}>
     <header className="ButtonText">Sign in</header>
   </button>;
 };
-=======
-  return <button onClick={() => loginWithRedirect()}>Log In</button>
-}
->>>>>>> backupidp
 
 export default LoginButton
--- pweb_frontend/src/index.js
+++ pweb_frontend/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
@@ -6,18 +5,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { Auth0Provider } from '@auth0/auth0-react'
->>>>>>> backupidp
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-<<<<<<< HEAD
   <BrowserRouter>
     <Auth0Provider
       domain="dev-rxvajvsu.us.auth0.com"
@@ -30,16 +20,6 @@ root.render(
     </Auth0Provider>
   </BrowserRouter>
 );
-=======
-  <Auth0Provider
-    domain="dev-rxvajvsu.us.auth0.com"
-    clientId="vqXaPyLnv7HeXY45yQNfxLdPrh93FFPw"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>
-)
->>>>>>> backupidp
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
