e5787771649534178f7ab90f0392d5ef75af1fe8
calinbucur <calinbucur8@gmail.com>
2022-05-21 17:20:40 +0300

    Added user profile page

--- pweb_frontend/src/Api.js
+++ pweb_frontend/src/Api.js
@@ -1,7 +1,16 @@
 const base = "http://localhost:5000/";
 const routes = {
-    owners: {
-        addOwner: "/api/v1/Owners/registerOwner"
+    owner: {
+        addowner: "/api/v1/Owners/registerOwner",
+        getowner: "/api/v1/Owners/getOwner",
+    },
+    foster: {
+        addfoster: 'api/v1/Fosters/registerFoster',
+        getfoster: '',
+    },
+    rescuer: {
+        addrescuer: '',
+        getrescuer: '',
     },
 };
 
